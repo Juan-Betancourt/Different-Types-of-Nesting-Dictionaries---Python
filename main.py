7a76d700416bd67e550ed312d852dc00015db683
Juan Betancourt <juancbetancourt11@gmail.com>
2022-05-16 01:10:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Nesting
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+#Nesting a list in a Dictionary
+
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stutgart"],
+}
+
+#Nesting a Dictionary in a Dictionary
+travel_log = {
+"France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total _visits": 12},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stutgart"], "total _visits": 5},
+}
+
+#Nesting a Dictionary in a list
